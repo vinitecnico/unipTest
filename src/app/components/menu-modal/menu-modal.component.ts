66332bf47c9672242afc4660493f4dfa517e4ff8
vinicius rodrigues <vinitecnico@gmail.com>
2019-06-15 23:40:08 +0100

    fix edit item

--- src/app/components/menu-modal/menu-modal.component.ts
+++ src/app/components/menu-modal/menu-modal.component.ts
@@ -6,9 +6,6 @@ import { finalize } from 'rxjs/operators';
 // Services
 import { MenuService } from 'src/app/services/menu.service';
 
-// Classes
-
-
 @Component({
     selector: 'app-menu-modal',
     templateUrl: './menu-modal.component.html',
@@ -37,7 +34,7 @@ export class MenuModalComponent implements OnInit {
     }
 
     ngOnInit() {
-        if (this.data && this.data.item && this.data.item.id && !this.data.item.isNew) {
+        if (this.data && this.data.item && this.data.item.id && !this.data.isNew) {
             this.menuService.getById(this.data.item.id)
                 .subscribe((response: any) => {
                     const data: any = {};
@@ -64,7 +61,6 @@ export class MenuModalComponent implements OnInit {
     }
 
     saveOrUpdate(id: number): void {
-        debugger
         if (!this.form.valid) {
             return;
         }
@@ -76,15 +72,29 @@ export class MenuModalComponent implements OnInit {
             }
 
             if (this.data.title == 'Sub-item') {
-                this.data.item.items.push({ item: { name: request.name, icon: request.icon } });
+                if (this.data.isNew) {
+                    this.data.item.items.push({ item: { name: request.name, icon: request.icon } });
+                } else {
+                    this.data.item.items[this.data.indexEdit].item.name = request.name;
+                    this.data.item.items[this.data.indexEdit].item.icon = request.icon;
+                }
             } else {
                 if (!this.data.item.items[this.data.index].listItem) {
                     this.data.item.items[this.data.index].listItem = [];
                 }
-                this.data.item.items[this.data.index].listItem.push({ name: request.name, icon: request.icon });
+                if (this.data.isNew) {
+                    this.data.item.items[this.data.index].listItem.push({ name: request.name, icon: request.icon });
+                } else {
+                    this.data.item.items[this.data.index].listItem[this.data.indexEdit].name = request.name;
+                    this.data.item.items[this.data.index].listItem[this.data.indexEdit].icon = request.icon;
+                }
             }
 
             request = this.data.item;
+        } else {
+            if (!this.data.item.isNew) {
+                request.id = id;
+            }
         }
 
         this.menuService.createOrUpdateMenu(request)
